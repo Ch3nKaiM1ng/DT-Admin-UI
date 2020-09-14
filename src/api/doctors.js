29dcc2736b8a68fe9f365b72117b2e0eff50a180
Lbi <lbi.goog@gmail.com>
2020-09-14 16:36:23 +0800

    git first

---
+++ src/api/doctors.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+
+export function DoctorSelect(data) {
+  // 所有医生列表
+  return request({
+    url: '/Doctor/findAll',
+    method: 'post',
+    data
+  })
+}
+
+export function Deldoctor(data) {
+  // 删除医生
+  return request({
+    url: '/Doctor/delObj',
+    method: 'post',
+    data
+  })
+}
+
+export function findObjdoctor(data) {
+  // 查询医生
+  return request({
+    url: '/Doctor/findObj',
+    method: 'post',
+    data
+  })
+}
+
+export function addObjdoctor(data) {
+  // 添加医生
+  return request({
+    url: '/Doctor/addObj',
+    method: 'post',
+    data
+  })
+}
+
+export function updateObjdoctor(data) {
+  // 修改医生
+  return request({
+    url: '/Doctor/updateObj',
+    method: 'post',
+    data
+  })
+}
+export function updateStatus(data) {
+  // 修改医生
+  return request({
+    url: '/Doctor/updateStatus',
+    method: 'post',
+    data
+  })
+}
